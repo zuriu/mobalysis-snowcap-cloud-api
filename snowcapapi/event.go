e119f0fe77163687726161b9d3133f6066243024
Bryan Callahan <bryancallahan@gmail.com>
2017-11-13 21:55:14 -0600

    Adding depthMin, depthMax, and description fields to events

--- snowcapapi/event.go
+++ snowcapapi/event.go
@@ -5,15 +5,18 @@ import (
 )
 
 type Event struct {
-	UUID      string    `json:"uuid"`
-	CreatedOn time.Time `json:"createdOn"`
-	TimeStamp time.Time `json:"timeStamp"`
-	Type      string    `json:"type"`
-	Depth     float64   `json:"depth"`
-	DumpDepth float64   `json:"dumpDepth"`
-	Temp      float64   `json:"temp"`
-	TempMin   float64   `json:"tempMin"`
-	TempMax   float64   `json:"tempMax"`
-	Pressure  float64   `json:"pressure"`
-	Humidity  float64   `json:"humidity"`
+	UUID        string    `json:"uuid"`
+	CreatedOn   time.Time `json:"createdOn"`
+	TimeStamp   time.Time `json:"timeStamp"`
+	Type        string    `json:"type"`
+	Depth       float64   `json:"depth"`
+	DumpDepth   float64   `json:"dumpDepth"`
+	DepthMin    float64   `json:"depthMin"`
+	DepthMax    float64   `json:"depthMax"`
+	Temp        float64   `json:"temp"`
+	TempMin     float64   `json:"tempMin"`
+	TempMax     float64   `json:"tempMax"`
+	Pressure    float64   `json:"pressure"`
+	Humidity    float64   `json:"humidity"`
+	Description string    `json:"description"`
 }
